8a2a75de6cf6c980d7102ebc36b8d7838a535d37
Evan Ordona <eordona@umd.edu>
2024-02-28 20:00:23 -0500

    fix link

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://mak-game.onrender.com/',
+        origin: 'https://mak-game.onrender.com/',
         methods: ['GET', 'POST'],
     }
 })
