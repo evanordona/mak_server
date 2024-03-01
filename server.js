0802e5d109592e12ca274414e3f68026b4773f80
Evan Ordona <eordona@umd.edu>
2024-03-01 02:01:35 -0500

    refactoring and comments

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const { Server } = require('socket.io')
 
 app.use(cors())
 
+// https://mak-game.onrender.com
+// http://localhost:5173
 const io = new Server(server, {
     cors: {
         origin: 'https://mak-game.onrender.com',
@@ -17,9 +19,7 @@ const io = new Server(server, {
     }
 })
 
-// Parses the text as url encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.get('/', (req, res) => {
     res.send("hello world!");
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 const rooms = {}
 
 io.on('connection', (socket) => {
-    
+
     // Hosting and joining room
     socket.on('joinRoom', (code) => {
         console.log(code);
@@ -43,29 +43,36 @@ io.on('connection', (socket) => {
             rooms[code] += 1
             if (rooms[code] == 2) {
                 io.to(code).emit('startGame')
-            } 
+            }
         }
         console.log(rooms[code])
     })
 
     socket.on('send-card', (card, code) => {
         console.log(card);
+
+        // send card to other player in room
         socket.to(code).emit("receive-card", card)
     })
 
-   
-    
 
     socket.on('disconnect', () => {
-        rooms[Object.keys(socket.rooms).filter(item => item!=socket.id)[0]] -= 1 
 
-        if (rooms[Object.keys(socket.rooms).filter(item => item!=socket.id)[0]] == 0) {
-            delete rooms[Object.keys(socket.rooms).filter(item => item!=socket.id)[0]]
+
+        // decrease room counts
+        const room = rooms[Object.keys(socket.rooms).filter(item => item != socket.id)]
+        if (room && room.length != 0) {
+            rooms[room[0]] -= 1
+
+            if (rooms[room[0]] == 0) {
+                delete rooms[Object.keys(socket.rooms).filter(item => item != socket.id)[0]]
+            }
         }
+
         console.log('user disconnected');
     });
 
-    
+
 })
 
 server.listen(port, () => {
